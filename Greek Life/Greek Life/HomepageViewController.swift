ba9fb549a5cd3cf8d0d77a0aa457f3c0a0568f5a
Jonah Elbaz <jonahelbaz@Jonahs-MacBook-Pro.local>
2017-10-28 18:26:30 -0400

    Replace labels with buttons (image part of button)

--- Greek Life/Greek Life/HomepageViewController.swift
+++ Greek Life/Greek Life/HomepageViewController.swift
@@ -11,28 +11,17 @@ import FirebaseAuth
 
 class HomepageViewController: UIViewController {
     
-    @IBOutlet weak var CalendarContainer: UILabel!
-    @IBOutlet weak var ForumContainer: UILabel!
-    @IBOutlet weak var PollContainer: UILabel!
-    @IBOutlet weak var BrothersContainer: UILabel!
-    @IBOutlet weak var InfoContainer: UILabel!
-    @IBOutlet weak var ProfileContainer: UILabel!
-    @IBOutlet weak var MasterContainer: UILabel!
-    @IBOutlet weak var imContainer: UILabel!
-    @IBOutlet weak var imLabel: UILabel!
+    @IBOutlet weak var ForumContainer: UIButton!
+    @IBOutlet weak var CalendarContainer: UIButton!
+    @IBOutlet weak var PollContainer: UIButton!
+    @IBOutlet weak var BrothersContainer: UIButton!
+    @IBOutlet weak var MasterContainer: UIButton!
+    @IBOutlet weak var ProfileContainer: UIButton!
+    @IBOutlet weak var imContainer: UIButton!
+    @IBOutlet weak var InfoContainer: UIButton!
     
     @IBOutlet weak var Logo: UIImageView!
     @IBOutlet weak var BackgroundPic: UIImageView!
-    
-    @IBOutlet weak var InstantMessaging: UIImageView!
-    @IBOutlet weak var Forum: UIImageView!
-    @IBOutlet weak var Brothers: UIImageView!
-    @IBOutlet weak var Profile: UIImageView!
-    @IBOutlet weak var Master: UIImageView!
-    @IBOutlet weak var Info: UIImageView!
-    @IBOutlet weak var Poll: UIImageView!
-    @IBOutlet weak var Calendar: UIImageView!
-    
     
     @IBAction func LogOut(_ sender: Any) {
         let firebaseAuth = Auth.auth()
@@ -49,57 +38,58 @@ class HomepageViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+       // let blue = UIColor.blue.withAlphaComponent(0.3)
         
-        Forum?.image = UIImage(named: "AEPiDocs/Icons/Forum.png")
-        InstantMessaging?.image = UIImage(named: "AEPiDocs/Icons/InstantMessaging.png")
-        Brothers?.image = UIImage(named: "AEPiDocs/Icons/Brothers.png")
-        Profile?.image = UIImage(named: "AEPiDocs/Icons/Profile.png")
-        Master?.image = UIImage(named: "AEPiDocs/Icons/Master.png")
-        Info?.image = UIImage(named: "AEPiDocs/Icons/Info.png")
-        Poll?.image = UIImage(named: "AEPiDocs/Icons/Poll.png")
-        Calendar?.image = UIImage(named: "AEPiDocs/Icons/Calendar.png")
-        BackgroundPic?.image = UIImage(named: "AEPiDocs/School.png");
+        BackgroundPic?.image = UIImage(named: "AEPiDocs/School.png")
         BackgroundPic?.alpha = 0.9
         Logo?.image = UIImage(named: "AEPiDocs/Logos/AEPi.png");
         
         imContainer.layer.cornerRadius = imContainer.frame.width/2;
         imContainer.layer.borderWidth = 0.5;
+        imContainer.setImage(UIImage(named: "AEPiDocs/Icons/InstantMessaging.png"), for: UIControlState.normal)
+        imContainer.imageEdgeInsets = UIEdgeInsetsMake(55,55,55,55)
+        //imContainer.layer.backgroundColor = blue.cgColor
+        
         CalendarContainer.layer.cornerRadius = CalendarContainer.frame.width/2;
         CalendarContainer.layer.borderWidth = 1;
+        CalendarContainer.setImage(UIImage(named: "AEPiDocs/Icons/Calendar.png"), for: UIControlState.normal)
+        CalendarContainer.imageEdgeInsets = UIEdgeInsetsMake(55,55,55,55)
+        //CalendarContainer.layer.backgroundColor = blue.cgColor
+
         ForumContainer.layer.cornerRadius = ForumContainer.frame.width/2;
         ForumContainer.layer.borderWidth = 1;
+        ForumContainer.setImage(UIImage(named: "AEPiDocs/Icons/Forum.png"), for: UIControlState.normal)
+        ForumContainer.imageEdgeInsets = UIEdgeInsetsMake(55,55,55,55)
+        //ForumContainer.layer.backgroundColor = blue.cgColor
+
         PollContainer.layer.cornerRadius = PollContainer.frame.width/2;
         PollContainer.layer.borderWidth = 1;
+        PollContainer.setImage(UIImage(named: "AEPiDocs/Icons/Poll.png"), for: UIControlState.normal)
+        PollContainer.imageEdgeInsets = UIEdgeInsetsMake(55,55,55,55)
+       // PollContainer.layer.backgroundColor = blue.cgColor
+
         BrothersContainer.layer.cornerRadius = BrothersContainer.frame.width/2;
         BrothersContainer.layer.borderWidth = 1;
+        BrothersContainer.setImage(UIImage(named: "AEPiDocs/Icons/Brothers.png"), for: UIControlState.normal)
+        BrothersContainer.imageEdgeInsets = UIEdgeInsetsMake(55,55,55,55)
+
         InfoContainer.layer.cornerRadius = InfoContainer.frame.width/2;
         InfoContainer.layer.borderWidth = 1;
-        
+        InfoContainer.setImage(UIImage(named: "AEPiDocs/Icons/Info.png"), for: UIControlState.normal)
+        InfoContainer.imageEdgeInsets = UIEdgeInsetsMake(55,55,55,55)
+       // InfoContainer.layer.backgroundColor = blue.cgColor
+
         ProfileContainer.layer.cornerRadius = ProfileContainer.frame.width/2;
         ProfileContainer.layer.borderWidth = 1;
+        ProfileContainer.setImage(UIImage(named: "AEPiDocs/Icons/Profile.png"), for: UIControlState.normal)
+        ProfileContainer.imageEdgeInsets = UIEdgeInsetsMake(55,55,55,55)
+        //ProfileContainer.layer.backgroundColor =  blue.cgColor
+        
         MasterContainer.layer.cornerRadius = MasterContainer.frame.width/2;
         MasterContainer.layer.borderWidth = 1;
-        
-        let blue = UIColor.blue.withAlphaComponent(0.3)
-        
-        MasterContainer.layer.backgroundColor = blue.cgColor
-        imContainer.layer.backgroundColor = blue.cgColor
-        CalendarContainer.layer.backgroundColor = blue.cgColor
-        ForumContainer.layer.backgroundColor = blue.cgColor
-        PollContainer.layer.backgroundColor = blue.cgColor
-        BrothersContainer.layer.backgroundColor = blue.cgColor
-        InfoContainer.layer.backgroundColor = blue.cgColor
-        ProfileContainer.layer.backgroundColor =  blue.cgColor
-        
-        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
-        Info.isUserInteractionEnabled = true
-        Info.addGestureRecognizer(tapGestureRecognizer)
-        
-    }
-    
-    func imageTapped(tapGestureRecognizer: UITapGestureRecognizer){
-        //let tappedImage = tapGestureRecognizer.view as! UIImageView
-        print("hi");
+        MasterContainer.setImage(UIImage(named: "AEPiDocs/Icons/Master.png"), for: UIControlState.normal)
+        MasterContainer.imageEdgeInsets = UIEdgeInsetsMake(55,55,55,55)
+       // MasterContainer.layer.backgroundColor = blue.cgColor
     }
 
     override func didReceiveMemoryWarning() {
