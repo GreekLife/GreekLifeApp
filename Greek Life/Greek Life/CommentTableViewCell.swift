d446717c90501e72d64dec654180e7f1d651089c
Jonah Elbaz <jonah-elbaz@hotmail.com>
2017-11-03 14:26:18 -0400

    Implement comment popup controller for forum

---
+++ Greek Life/Greek Life/CommentTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CommentTableViewCell.swift
+//  Greek Life
+//
+//  Created by Jonah Elbaz on 2017-11-03.
+//  Copyright © 2017 ConcordiaDev. All rights reserved.
+//
+
+import UIKit
+
+class CommentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Greek Life/Greek Life/CommentViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CommentViewController.swift
+//  Greek Life
+//
+//  Created by Jonah Elbaz on 2017-11-03.
+//  Copyright © 2017 ConcordiaDev. All rights reserved.
+//
+
+import UIKit
+
+class CommentViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(displayP3Red: 2/255, green: 0/255, blue: 176/255, alpha: 0.1)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return(0)
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CommentCell", for: indexPath) as! ForumCellTableViewCell
+        return(cell)
+    }
+
+
+}
