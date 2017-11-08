efcccfe34f87a2cf230b692aeb86091abc2b063e
Jonah Elbaz <jonah-elbaz@hotmail.com>
2017-11-07 19:10:38 -0500

    Setting up placeholders for brother branch

---
+++ Greek Life/Greek Life/MemberCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MemberCollectionViewCell.swift
+//  Greek Life
+//
+//  Created by Jonah Elbaz on 2017-11-07.
+//  Copyright © 2017 ConcordiaDev. All rights reserved.
+//
+
+import UIKit
+
+class MemberCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var MemberImage: UIImageView!
+    @IBOutlet weak var MemberName: UILabel!
+    @IBOutlet weak var MemberSpecialty: UILabel!
+    
+}
