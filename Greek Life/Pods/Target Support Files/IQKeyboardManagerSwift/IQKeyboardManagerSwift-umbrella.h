197bc3d77beaf481ecef3e3a408d47163f48b07e
Jonah Elbaz <jonah-elbaz@hotmail.com>
2017-11-08 20:01:03 -0500

    Add new dependancy --> IQKeyboardManager for Swift.

--- Greek Life/Pods/Target Support Files/IQKeyboardManagerSwift/IQKeyboardManagerSwift-umbrella.h
+++ Greek Life/Pods/Target Support Files/IQKeyboardManagerSwift/IQKeyboardManagerSwift-umbrella.h
@@ -10,9 +10,7 @@
 #endif
 #endif
 
-#import "GTMDefines.h"
-#import "GTMNSData+zlib.h"
 
-FOUNDATION_EXPORT double GoogleToolboxForMacVersionNumber;
-FOUNDATION_EXPORT const unsigned char GoogleToolboxForMacVersionString[];
+FOUNDATION_EXPORT double IQKeyboardManagerSwiftVersionNumber;
+FOUNDATION_EXPORT const unsigned char IQKeyboardManagerSwiftVersionString[];
 
