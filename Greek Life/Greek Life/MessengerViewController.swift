4106ac68b04e0b45d538eb5e964916a9cb3dde3d
Jon Zlotnik <j.zlotnik@live.ca>
2017-12-20 23:38:16 -0500

    help please

--- Greek Life/Greek Life/MessengerViewController.swift
+++ Greek Life/Greek Life/MessengerViewController.swift
@@ -9,14 +9,83 @@
 import UIKit
 import FirebaseDatabase
 
-  //***********************************//
- //  Messenger Classes: Channels, DM //
-//***********************************//
-class Channel
+  //*****************************************//
+ //  Messenger Model Objects: Channels, DM  //
+//********************************888888***//
+struct Channel
 {
     var title = ""
     var users = [String:Any]()
     var messages = [String:String]()
+}
+struct DMessenger {
+    static var dmList = [DM]()
+    static func loadDMList(dataRef:DatabaseReference) -> Void {
+        dataRef.child("Messenger/DMs").observe(.value, with: {(snapshot) in
+            for dmSnap in snapshot.children {
+                let dmSnapshot = dmSnap as! DataSnapshot
+                let messageDict = dmSnapshot.childSnapshot(forPath: "Messages").value as! [String:String]
+                let dmID = dmSnapshot.childSnapshot(forPath: "dmID").value as! String
+                let dmEntry = DM(id: dmID, messages: messageDict)
+                self.dmList.append(dmEntry)
+            }
+        })
+    }
+    static func initUser(dataRef:DatabaseReference) -> Void {
+    //Check to make sure user has a dm with every other user.
+    //If not, create dm in db and put welcome message.
+        let LoggedInUserID = LoggedIn.User["UserID"] as! String
+        var userIDList:[String] = []
+        var dmIDList:[String] = []
+        //Get list of userID's
+        dataRef.child("Users").observe(.value, with: { (snapshot) in
+            
+            userIDList = (snapshot.value as! [String:Any]).keys.reversed()
+            
+        })
+        //Get list of dmID's
+        for dm in DMessenger.dmList {
+            dmIDList.append(dm.dmID)
+        }
+        //Check to see if logged in user has a dm with every other user
+        
+        for userID in userIDList {
+            if (userID != LoggedInUserID)
+            {
+                var dmWithUserExists = false
+                for dmID in dmIDList{
+                    if (dmID.contains(LoggedInUserID) && dmID.contains(userID)){
+                        dmWithUserExists = true
+                    }
+                }
+                //If dm between logged in user and userID doesn't exist then make one
+                //and display welcome message
+                if !dmWithUserExists {
+                    let timeStamp = String(Date.init().timeIntervalSince1970)
+                    dataRef.child("Messenger/DMs/"+userID+LoggedInUserID+"/Messages/"+timeStamp+","+LoggedInUserID).setValue("Hey, it's brother "+(LoggedIn.User["BrotherName"] as! String))
+                }
+            }
+        }
+        
+        DMessenger.loadDMList(dataRef: dataRef)
+    }
+}
+class DM {
+    var dmID:String
+    var messages:[String:String]
+    init(id dmID:String, messages:[String:String]) {
+        self.dmID = dmID
+        self.messages = messages
+    }
+    func sendMessage(senderUserID:String, message:String, dataRef:DatabaseReference, sendingView:UIViewController) -> Void {
+        let messageID = String(Date.init().timeIntervalSince1970) + "," + senderUserID
+        dataRef.child("Messenger/DMs/"+dmID+"/"+messageID).setValue(message){ error in
+            let alert = UIAlertController(title: "Cannot send...", message: "We were unable to send you message, please check your internet connection.", preferredStyle: UIAlertControllerStyle.alert )
+            alert.addAction(UIAlertAction(title: "Lemme try again", style: UIAlertActionStyle.cancel, handler: { (action) in
+                alert.dismiss(animated: true, completion: nil)
+            }))
+        }
+    }
 }
 
   //***********************************//
@@ -87,6 +156,8 @@ class ChannelViewController: UIViewController, UITableViewDelegate, UITableViewD
 //******************************//
 class DMViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
 {
+    //DataBaseStuff
+    let dataRef = Database.database().reference()
     
     //Top toolbar
     @IBAction func backBTN(_ sender: Any)
@@ -115,7 +186,11 @@ class DMViewController: UIViewController, UITableViewDelegate, UITableViewDataSo
     func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
         performSegue(withIdentifier: "ChatSettingsSegue", sender: indexPath.row)
     }
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        DMessenger.loadDMList(dataRef: dataRef)
+        DMessenger.initUser(dataRef: dataRef)
+    }
 }
 
 
