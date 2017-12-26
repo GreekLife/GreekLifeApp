03280406d7297ed2a2124adc6f33b17b23c563b8
Jon Zlotnik <j.zlotnik@live.ca>
2017-12-25 23:31:53 -0500

    Beginning of cleaner IM

--- Greek Life/Greek Life/MessengerViewController.swift
+++ Greek Life/Greek Life/MessengerViewController.swift
@@ -6,134 +6,140 @@
 //  Copyright © 2017 ConcordiaDev. All rights reserved.
 //
 
+/*Avi was here in this code. hahahahaha
+ Enter block Jonah enter {
+ {I am the best
+ When you press enter your message sends
+ When you type it types the message
+ That is how a messenger works
+ Thank you computer for doing
+ The end
+ Error be gone
+ 
+ }*/
+
+
 import UIKit
 import FirebaseDatabase
 
-  //*****************************************//
- //  Messenger Model Objects: Channels, DM  //
-//********************************888888***//
-struct Channel
-{
-    var title = ""
-    var users = [String:Any]()
-    var messages = [String:String]()
-}
-struct DMessenger {
-    static var dmList = [DM]()
-    static var dmListOfLoggedIn = [DM]()
+//-----------------------------------------------------------------------------------------------------------
+//  Database Objects
+//-----------------------------------------------------------------------------------------------------------
+
+struct DatabaseHousekeeping {
     
-    static func initUser(for dmTable:UITableView) -> Void {
-    //Check to make sure user has a dm with every other user.
-    //If not, create dm in db and put welcome message.
-        var dataRef = Database.database().reference()
-        let LoggedInUserID = LoggedIn.User["UserID"] as! String
-        var userIDList:[String] = []
-        var dmIDList:[String] = []
-        dmList.removeAll()
-        dmListOfLoggedIn.removeAll()
-        //Load up the dm's from db
-        Database.database().reference().child("Messenger/DMs").observe(.value, with: {(snapshot) in
-            for dmSnap in snapshot.children {
-                let dmSnapshot = dmSnap as! DataSnapshot
-                let messageDict = dmSnapshot.childSnapshot(forPath: "Messages").value as! [String:String]
-                let dmID = dmSnapshot.key// childSnapshot(forPath: "dmID").value as! String
-                let dmEntry = DM(id: dmID, messages: messageDict)
-                self.dmList.append(dmEntry)
-            }
-        
-        //Get list of userID's
-        Database.database().reference().child("Users").observe(.value, with: { (snapshot) in
-            
-            userIDList = (snapshot.value as! [String:Any]).keys.reversed()
-            //Replace Master key with his actual userID
-            userIDList[userIDList.index(of: "Master")!] = snapshot.childSnapshot(forPath: "Master/UserID").value as! String
-            
-            //Get list of dmID's
-            for dm in DMessenger.dmList {
-                dmIDList.append(dm.dmID)
-            }
-            //Check to see if logged in user has a dm with every other user
-        
-            for userID in userIDList {
-                if (userID != LoggedInUserID)
-                {
-                    var dmWithUserExists = false
-                    for dmID in dmIDList{
-                        if (dmID.contains(LoggedInUserID) && dmID.contains(userID)){
-                            dmWithUserExists = true
-                        }
-                    }
-                    //If dm between logged in user and userID doesn't exist then make one
-                    //and display welcome message
-                    if !dmWithUserExists {
-                        dataRef = Database.database().reference()
-                        let timeStamp = String(Int(Date.init().timeIntervalSince1970))
-                        dataRef.child("Messenger/DMs/"+userID+LoggedInUserID+"/Messages/"+timeStamp+","+LoggedInUserID).setValue("Hey, it's brother "+(LoggedIn.User["BrotherName"] as! String))
-                    }
-                }
-            }
-            for dm in dmList {
-                if dm.dmID.contains(LoggedIn.User["UserID"] as! String) && dm.dmID != dm.dmID+dm.dmID {
-                    dmListOfLoggedIn.append(dm)
-                }
-            }
-            dmTable.reloadData()
-            })
-        })
-    }
-}
-class DM {
-    var dmID:String
-    var messages:[String:String]
-    init(id dmID:String, messages:[String:String]) {
-        self.dmID = dmID
-        self.messages = messages
-    }
-    func sendMessage(senderUserID:String, message:String, sendingView:UIViewController) -> Void {
-        let dataRef = Database.database().reference()
-        let messageID = String(Date.init().timeIntervalSince1970) + "," + senderUserID
-        dataRef.child("Messenger/DMs/"+dmID+"/"+messageID).setValue(message){ error in
-            let alert = UIAlertController(title: "Cannot send...", message: "We were unable to send you message, please check your internet connection.", preferredStyle: UIAlertControllerStyle.alert )
-            alert.addAction(UIAlertAction(title: "Lemme try again", style: UIAlertActionStyle.cancel, handler: { (action) in
-                alert.dismiss(animated: true, completion: nil)
-            }))
-        }
-    }
+    static var handles = [DatabaseHandle]()
+    
 }
 
-  //***********************************//
- //  Channel View Controller Class    //
-//***********************************//
+//-----------------------------------------------------------------------------------------------------------
+//  Messenger Objects
+//-----------------------------------------------------------------------------------------------------------
+
+// --- Channel Dialiogue --- //
+
+class ChannelDialogue {
+    
+    // --- Channel Dialogue Attributes --- //
+    
+    var messengees = [Messengee]()
+    var messages = [Message]()
+    
+}
+
+// --- Direct Dialogue --- //
+
+class DirectDialogue {
+    
+    // --- Direct Messenger Dialogue Properties --- //
+    
+    var id = ""
+    var messages = [Message]()
+    var messengees = [Messengee]()
+    
+    // --- Constructor ---//
+    
+    init(id:String) {
+        self.id = id
+        // If the direct dialoue exists pull the data.
+        // Otherwise, create a new one with a welcome message from both messengees
+        let handle = Database.database().reference().child("DirectDialogues/"+id).observe(.value, with: { snapshot in
+            let messengeeIDsString = snapshot.childSnapshot(forPath: "Messengees").value as! String
+            let messengeeIDs = messengeeIDsString.components(separatedBy: ", ")
+            //Pulling messengee data
+            for id in messengeeIDs {
+                self.messengees.append(Messengee(userID: id))
+            }
+        }){ error in
+            Database.database().reference().child("DirectDialogues/"+id)
+        }
+        DatabaseHousekeeping.handles.append(handle)
+    }
+    init(ids: [String]) {
+        //Put together a proper DirectDialogue ID
+    }
+    
+}
+
+
+// --- Messengee --- //
+
+class Messengee {
+    
+    // --- Messengee Properties --- //
+    
+    var firstName = ""
+    var lastName = ""
+    var userID = ""
+    var brotherName = ""
+    var position = ""
+    
+    // --- Contstructor --- //
+    
+    init(userID:String) {
+        self.userID = userID
+        let handle = Database.database().reference().child("Users/"+userID).observe(.value, with: { (snapshot) in
+            self.firstName = snapshot.value(forKey: "First Name") as! String;
+            self.lastName = snapshot.value(forKey: "Last Name") as! String;
+            self.brotherName = snapshot.value(forKey: "BrotherName") as! String;
+            self.position = snapshot.value(forKey: "Position") as! String;
+        }){ error in
+            print("brother did not exist")
+        }
+        DatabaseHousekeeping.handles.append(handle)
+    }
+    
+}
+
+// --- Message --- //
+
+class Message {
+    
+    // --- Message Properties --- //
+    
+    var id = ""
+    var content = ""
+    var sentBy = ""
+    var timeSent = ""
+    
+    // --- Constructor --- //
+    
+    init(id:String, content:String) {
+        self.id = id
+        self.content = content
+    }
+    
+}
+
+
+//-----------------------------------------------------------------------------------------------------------
+//  Channels View
+//-----------------------------------------------------------------------------------------------------------
+
+//--- Channels Controller ---//
+
 class ChannelViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
 {
-    var dataRef = Database.database().reference()
-    var dataRef2 = Database.database().reference()
-    
-    var channels = [Channel]()
-    
-    func initChannels () {
-        //Getting Channel Users
-        self.dataRef.child("Messenger").child("Channels").observeSingleEvent(of: .value, with: {(snapshot) in
-            let dbChannels = snapshot.value as? NSDictionary
-            for channel in dbChannels! {
-                var currentChannel:Channel = Channel()
-                currentChannel.title = channel.key as! String
-                let dbMessages = snapshot.childSnapshot(forPath: "\(currentChannel.title)/Messages").value as? NSDictionary
-                print(dbMessages)
-            }
-            
-        })
-        { (error) in
-            print("Could not retrieve object from database");
-        }
-    }
-    override func viewDidLoad() {
-        initChannels()
-    }
-    
-    
-    
-    
     //Top toolbar
     @IBAction func backBTN(_ sender: Any)
     {
@@ -153,7 +159,6 @@ class ChannelViewController: UIViewController, UITableViewDelegate, UITableViewD
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
     {
-        
         performSegue(withIdentifier: "ChatViewSegue", sender: indexPath.row)
     }
     func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
@@ -163,70 +168,104 @@ class ChannelViewController: UIViewController, UITableViewDelegate, UITableViewD
     
     
 }
-  //******************************//
- //  DM View Controller Class    //
-//******************************//
+
+
+
+//-----------------------------------------------------------------------------------------------------------
+//  Direct Messaging
+//-----------------------------------------------------------------------------------------------------------
+
+//--- Direct Messaging Controller ---//
+
 class DMViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
 {
-    //DataBaseStuff
-    let dataRef = Database.database().reference()
-    var otherUsersFirstLast = [String:String]()
     
-    //Top toolbar
+    //-- IB Outlets and Actions --//
+    
     @IBAction func backBTN(_ sender: Any)
     {
         presentingViewController?.dismiss(animated: true)
     }
-   
     
-    
-    //Table Stuff
-    @IBOutlet weak var dmTableView: UITableView!
+    //-- Table of Direct Messaging Conversations --//
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        return DMessenger.dmListOfLoggedIn.count
+        return 0
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
-        let dmCell = UITableViewCell()
-        var otherBrotherID = ""
-        if let userIDRange = DMessenger.dmListOfLoggedIn[indexPath.row].dmID.range(of: LoggedIn.User["UserID"] as! String){
-            otherBrotherID = DMessenger.dmListOfLoggedIn[indexPath.row].dmID
-            otherBrotherID.removeSubrange(userIDRange)
-        }
-        
-        dmCell.textLabel?.text = self.otherUsersFirstLast[otherBrotherID]
-        return dmCell
+        return UITableViewCell()
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
     {
-        
-        performSegue(withIdentifier: "ChatViewSegue", sender: indexPath.row)
+        performSegue(withIdentifier: "ChatViewSegue", sender: "")
     }
     func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
-        performSegue(withIdentifier: "ChatSettingsSegue", sender: indexPath.row)
+        performSegue(withIdentifier: "ChatSettingsSegue", sender: "")
     }
+    
+    
+    //-- Prepare for Segues --//
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+    
+    
+    //-- View Did Load  --//
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        Database.database().reference().child("Users").observe(.value, with: { (snapshot) in
-            for userSnap in snapshot.children {
-                let userID = (userSnap as! DataSnapshot).childSnapshot(forPath: "UserID").value as! String
-                let firstName = (userSnap as! DataSnapshot).childSnapshot(forPath: "First Name").value as! String
-                let lastName = (userSnap as! DataSnapshot).childSnapshot(forPath: "Last Name").value as! String
-                self.otherUsersFirstLast[userID] = firstName+", "+lastName
-            }
-            DMessenger.initUser(for: self.dmTableView)
-        })
         
     }
 }
 
 
 
-  //***********************************//
- //   Settings View Controller Class  //
-//***********************************//
+//-----------------------------------------------------------------------------------------------------------
+//  Messaging Interface
+//-----------------------------------------------------------------------------------------------------------
+
+class ChatViewController: UIViewController,UITableViewDataSource,UITableViewDelegate{
+    
+    
+    //--- IB Outlets and Actions ---//
+    
+    @IBAction func backBTN(_ sender: Any)
+    {
+        presentingViewController?.dismiss(animated: true)
+    }
+    
+    
+    //--- Table of Messages in Direct Message or Channel ---//
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+    
+}
+
+
+//--- Cells for the Messaging Interface Table ---//
+
+class ChatViewMessageCell: UITableViewCell {
+    @IBOutlet weak var messageSender: UILabel!
+    @IBOutlet weak var messageContent: UILabel!
+    
+}
+
+
+//-----------------------------------------------------------------------------------------------------------
+//  Messaging Interface Settings
+//-----------------------------------------------------------------------------------------------------------
+
 class ChatSettingsViewController:UIViewController
 {
     @IBAction func doneBTN(_ sender: Any)
@@ -235,29 +274,6 @@ class ChatSettingsViewController:UIViewController
     }
     
 }
-
-  //*******************************//
- //   Chat View Controller Class  //
-//*******************************//
-class ChatViewController:
-    UIViewController,UITableViewDataSource,UITableViewDelegate
-{
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let messageCell = tableView.dequeueReusableCell(withIdentifier: "messageCell", for: indexPath)
-        return messageCell
-    }
-    
-    @IBAction func backBTN(_ sender: Any)
-    {
-        presentingViewController?.dismiss(animated: true)
-    }
-}
-
-
 
 
 
